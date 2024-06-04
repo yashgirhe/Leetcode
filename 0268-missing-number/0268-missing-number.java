03d3cdf8da9916df36bcff3c01eec975f3c3e767
yashgirhe <57618896+yashgirhe@users.noreply.github.com>
2024-06-04 20:18:44 +0530

    Time: 0 ms (100.00%), Space: 45.2 MB (34.17%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,24 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int[] hashArr = new int[max(nums) + 1];
-        for (int i = 0; i < nums.length; i++) {
-            hashArr[nums[i]]++;
+        int len = nums.length;
+        int expectedSum = len*(len+1)/2;
+        int sum = 0;
+        for(int i=0; i<len; i++){
+            sum += nums[i];
         }
-        int i=0;
-        for (i = 0; i < hashArr.length; i++) {
-            if (hashArr[i] == 0) {
-                return i;
-            }
-        }
-        return i;
-    }
-    public static int max ( int[] a){
-        int max = a[0];
-        for (int i = 1; i < a.length; i++) {
-            if (a[i] > max) {
-                max = a[i];
-            }
-        }
-        return max;
+        return expectedSum-sum;
     }
 }
