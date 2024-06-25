9d13b94bf030ae89e6de953a0103ce3ca77ccdec
yashgirhe <57618896+yashgirhe@users.noreply.github.com>
2024-06-25 13:49:10 +0530

    Time: 1 ms (67.08%), Space: 41.4 MB (64.68%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,20 +1,6 @@
 class Solution {
     public String interpret(String command) {
-         StringBuffer sb = new StringBuffer();
-        int i = 0;
-        while (i < command.length()) {
-            if (command.charAt(i) == 'G') {
-                sb.append("G");
-                i++;
-            }
-            else if (command.charAt(i) == '(' && command.charAt(i + 1) == ')') {
-                sb.append("o");
-                i += 2;
-            } else {
-                sb.append("al");
-                i += 4;
-            }
-        }
-        return sb.toString();
+        command = command.replace("(al)","al").replace("()","o");
+        return command;
     }
 }
